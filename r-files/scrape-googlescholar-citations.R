737fcbc197f9796ea66b374c63751586bf51c4fc
Henrik Kenneth Andersen <handersen1987@gmail.com>
2021-07-09 12:43:19 +0200

    Add an r-files folder

---
+++ r-files/scrape-googlescholar-citations.R
@@ -0,0 +1,33 @@
+
+# install.packages("scholar")
+library(scholar)
+
+# Get overview of publications
+# get_publications("bWQX7N8AAAAJ")
+# get_article_cite_history("bWQX7N8AAAAJ", "d1gkVwhDpl0C")
+
+# Get overview of citations per year
+citations <- get_citation_history("bWQX7N8AAAAJ")
+# citations
+
+# Function to round up 
+roundUpNice <- function(x, nice = c(1,2,4,5,6,8,10)) {
+  if(length(x) != 1) stop("'x' must be of length 1")
+  10^floor(log10(x)) * nice[[which(x <= 10^floor(log10(x)) * nice)[[1]]]]
+}
+
+# ---- cite-plot ---- 
+
+# Plot citations 
+library(ggplot2)
+
+cite_plot <- ggplot(citations, aes(x = year, y = cites)) + 
+  geom_col(color = "black", fill = "deepskyblue4") + 
+  geom_text(aes(label = cites), vjust = -0.5) + 
+  scale_y_continuous(name = "Citations", 
+                     limits = c(0, roundUpNice(max(citations$cites))), 
+                     breaks = seq(0, roundUpNice(max(citations$cites)), 5)) + 
+  scale_x_continuous(name = "Year") + 
+  ggtitle("Google Scholar citations per year for Henrik K. Andersen")
+
+print(cite_plot)
